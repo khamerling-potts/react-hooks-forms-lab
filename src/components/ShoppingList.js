22b13133f7ce3ba48865365f0b8a3c9149c3118f
Kyra Hamerling-Potts <khamerling-potts@wustl.edu>
2023-10-10 17:45:28 -0400

    filter working

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -4,22 +4,33 @@ import Filter from "./Filter";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
-  const [selectedCategory, setSelectedCategory] = useState("All");
+  // const [selectedCategory, setSelectedCategory] = useState("All");
+  const [formData, setFormData] = useState({
+    filter: "All",
+    search: "",
+  });
 
-  function handleCategoryChange(event) {
-    setSelectedCategory(event.target.value);
+  function handleChange(event) {
+    const name = event.target.name;
+    const value = event.target.value;
+    setFormData({ ...formData, [name]: value });
   }
 
   const itemsToDisplay = items.filter((item) => {
-    if (selectedCategory === "All") return true;
-
-    return item.category === selectedCategory;
+    if (formData.filter === "All") return item.name.includes(formData.search);
+    return (
+      item.name.includes(formData.search) && item.category === formData.filter
+    );
   });
 
   return (
     <div className="ShoppingList">
       <ItemForm />
-      <Filter onCategoryChange={handleCategoryChange} />
+      <Filter
+        onCategoryChange={handleChange}
+        onSearchChange={handleChange}
+        search={formData.search}
+      />
       <ul className="Items">
         {itemsToDisplay.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
